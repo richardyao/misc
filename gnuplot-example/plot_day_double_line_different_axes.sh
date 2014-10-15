285187fd6dd826e2e92fba37460c380a39750219
Richard Yao <richardyao1985@gmail.com>
2014-10-16 02:09:41 +0800

    aaaaa

--- gnuplot-example/plot_day_double_line_different_axes.sh
+++ gnuplot-example/plot_day_double_line_different_axes.sh
@@ -19,8 +19,8 @@ set grid
 set border 31-8-2 lt -1
 #set object 1 rectangle from graph 0,graph 0 to graph 1,graph 1 lw 5
 #set object 1 rectangle from graph 0, graph 0 to graph 1,graph 0.005 lw 1 fillcolor lt 2 border lc lt 2
-set object 1 rectangle from graph 0.998,graph 0 to graph 1,graph 1 lw 2 fs empty border lc lt 2
-set object 2 rectangle from graph 0,graph 0 to graph 0.002,graph 1 lw 2 fs empty border lc lt 1
+set object 1 rectangle from graph 0.998,graph 0 to graph 1,graph 1 lw 1 fs empty border lc lt 2
+set object 2 rectangle from graph 0,graph 0 to graph 0.002,graph 1 lw 1 fs empty border lc lt 1
 set title "2014-10-13 随机数曲线"
 set key right top
 plot "./day_double_line_0_20141013.data" using 1:3 title "QPS" with lines axes x1y1, "./day_double_line_1_20141013.data" using 1:3 title "成功率" with lines axes x1y2
