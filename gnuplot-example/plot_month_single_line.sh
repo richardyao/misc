69037266deca8856bc5b0d6f32837a4dbd714801
Richard Yao <richardyao1985@gmail.com>
2014-10-16 00:12:44 +0800

    make it bettter

--- gnuplot-example/plot_month_single_line.sh
+++ gnuplot-example/plot_month_single_line.sh
@@ -6,13 +6,13 @@ set output "./month_single_line_20141013.png"
 set xdata time
 set timefmt "%Y-%m-%d" 
 set format x "%m-%d"
-set ylabel "随机数 (个)" offset character 7,graph 0.57 rotate by 0 
+set ylabel "某数据 (个)" offset character 7,graph 0.57 rotate by 0 
 #set xtics 172800
 set xtics "2014-09-13",259200
 set xrange ["2014-09-13":"2014-10-13"]
 #set yrange [0:]
 set grid
-set title "2014-10-13 随机数曲线"
+set title "2014-10-13 某数据曲线"
 set key right top
 plot "./month_single_line_20141013.data" using 1:2 title "" with lines
 EOF
