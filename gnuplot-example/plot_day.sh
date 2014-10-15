4a5a1727092e3d1e4dcb41685450f022f0572099
Richard Yao <richardyao1985@gmail.com>
2014-10-15 23:19:15 +0800

    rename daily.sh to plot_day.sh

--- gnuplot-example/plot_day.sh
+++ gnuplot-example/plot_day.sh
@@ -2,7 +2,7 @@
 
 gnuplot<<EOF
 set terminal png font "./msyh.ttf" size 1000,400
-set output "./daily_20141013.png"
+set output "./day_20141013.png"
 set xdata time
 set timefmt "%Y-%m-%d %H:%M" 
 set format x "%H:%M"
@@ -10,5 +10,5 @@ set ylabel "随机数 (个)" offset graph 0.1,graph 0.57 rotate by 0
 set grid
 set title "2014-10-13 随机数曲线"
 set key right top
-plot "./daily_20141013.data" using 1:3 title ""
+plot "./day_20141013.data" using 1:3 title ""
 EOF
